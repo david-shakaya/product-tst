dcff1fbf62bda04277c035f127e4757c26708596
david-shakaya <devidoz666@gmail.com>
2021-03-28 19:00:16 +0300

    Логика для инпута с типом дата

--- src/js/index.js
+++ src/js/index.js
@@ -100,3 +100,14 @@ function minusValueInput() {
 
 selectColor();
 selectSize();
+
+function setDateNowInput() {
+    let today = new Date();
+    const dd = String(today.getDate()).padStart(2, '0');
+    const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    const yyyy = today.getFullYear();
+
+    today = yyyy + '-' + mm + '-' + dd;
+    return (formRef[1].value = today);
+}
+setDateNowInput();
